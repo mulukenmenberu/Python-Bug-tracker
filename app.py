f1ec3d530d681107f0e1d562b8b40a619edd1872
Muluken Menberu <mulerapps@gmail.com>
2022-09-26 23:39:20 +0300

    ...

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 from models.todo_model import db, Todo
+
 from db_operations import add_data, get_data, delete_data, update_data
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,11 +12,18 @@ def index():
 
 @app.route('/get_todos', methods=['GET'])
 def get_todo():
-    return render_template('index.html')
+    data = get_data()
+    return data
 
 @app.route('/add_todo', methods=['POST'])
 def add_todo():
-    return render_template('index.html')
+    todo_sata = request.get_json()
+    name = todo_sata['name']
+    description = todo_sata['description']
+    priority = todo_sata['priority']
+    print(name,description, priority)
+    add_data({"name":name,"description":description, "priority":priority})
+    return({"message":"success"})
 
 @app.route('/delete_todo', methods=['DELETE'])
 def delete_todo():
--- db_operations.py
+++ db_operations.py
@@ -1,14 +1,27 @@
 from models.todo_model import Todo, db
 from flask import jsonify
 def add_data(todo):
-    add_todo_data = Todo(name=todo.name, description=todo.description, priority=todo.priority, status=1)
+    add_todo_data = Todo(name=todo['name'], description=todo['description'], priority=todo['priority'], status=1)
     db.session.add(add_todo_data)
     db.session.commit()
     return jsonify({"message":"success"})
 
+def format_todos(todo):
+    todo_obj = []
+    for x in todo:
+        todo_obj.append({
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'priority': x.priority
+            })
+    return todo_obj
+
 def get_data():
     get_todo_data = Todo.query.all()
-    return jsonify(get_todo_data)
+    formated_todo = format_todos(get_todo_data)
+    print(formated_todo)
+    return jsonify(formated_todo)
 
 def delete_data(todo):
     Todo.query.filter(Todo.id == todo.id).delete()
